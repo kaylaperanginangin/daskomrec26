5e6b0c0ab261be3a9763462ab41460533bc38405
marzkigun27 <umarkigun@gmail.com>
2025-12-23 17:56:33 +0700

    project setup

---
+++ resources/js/Pages/welcome.jsx
@@ -0,0 +1,8 @@
+export default function welcome() {
+    return (
+        <div className="text-center">
+            <h1 className="text-4xl font-bold mb-4">Welcome to the Inertia.js React App!</h1>
+            <p className="text-lg">This is the welcome page.</p>
+        </div>
+    );
+}
